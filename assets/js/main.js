07329c639f9dab26be194a2d300762201c6660cc
Mr Amir <138612293+1234MrAmir@users.noreply.github.com>
2024-04-21 23:16:26 +0530

    seven commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -272,7 +272,7 @@ $(document).on("click", "#send-it", function() {
       )
     )
       var d = "whatsapp://send";
-    var g = d + "?phone= 8218752418" + e + f;
+    var g = d + "?phone=8218752418" + e + f;
     window.open(g, "_blank");
   }
 }),
